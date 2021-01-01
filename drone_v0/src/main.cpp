d76e92a7e882804f7efc2e4bed0686ac603290c3
danielggc <danielgrecia7@gmail.com>
2021-01-01 12:22:03 -0500

    terminando parte PWM motor

--- drone_v0/src/main.cpp
+++ drone_v0/src/main.cpp
@@ -21,6 +21,10 @@ using namespace std;
 
 int main(){ 
     DDRD |= (1<<PORTD2);
+    DDRD |= (1<<PORTD5);
+    DDRD |= (1<<PORTD6);
+    PORTD &= ~(1<<PORTD5);
+    PORTD &= ~(1<<PORTD6);
     for(int d=0;d<7;d++){
         _delay_ms(1000);
         PORTD &= ~(1<<PORTD2);
@@ -33,9 +37,20 @@ int main(){
     TCCR1A |=  (1 << COM1A1) | (0 << COM1A0) ; 
     TCCR1A |=  (0 << WGM11) | (1 << WGM10) ;
     TCCR1B |= (0 << CS12) | (1 << CS11) | ( 0 << CS10 );
-    OCR1A=255;
     TCCR1A |=  (1 << COM1B1)| (0 << COM1B0);
-    OCR1B=255;
+    OCR1A=50;
+    OCR1B=50;
+
+
+
+    TCCR0A |=  (1 << COM1A1) | (0 << COM1A0) ;
+    TCCR0A |=  (0 << WGM11) | (1 << WGM10) ;
+    TCCR0B |=  (0 << CS12) | (1 << CS11) | ( 0 << CS10 );
+    TCCR0A |=  (1 << COM1B1)| (0 << COM1B0);
+
+    OCR0A=100;
+    OCR0B=50;
+
 }
 
 
